46e9f81b196d3ba4af6a5fecd3bd341c277220f7
imacol <redimacol.re@gmail.com>
2026-04-17 15:33:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ const reportRoutes = require('./routes/reports');
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Database Configuration
-mongoose.connect('mongodb+srv://rooot:TU_PASSWORD@gestor-pagos.rbbjl7w.mongodb.net/gestor-pagos', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://admin:978331Abi*-@@gestor-pagos.rbbjl7w.mongodb.net/gestor-pagos', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected!'))
     .catch(err => console.log(err));
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/reports', reportRoutes);
 
-// Server Setup
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
