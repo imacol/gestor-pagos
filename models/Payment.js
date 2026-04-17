b55745b7b13a735da2035b25d20c55d9a8bc2a33
imacol <redimacol.re@gmail.com>
2026-04-17 13:18:45 -0500

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,1 +1,32 @@
+const mongoose = require('mongoose');
 
+const PaymentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        default: 'general'
+    },
+    status: {
+        type: String,
+        enum: ['pendiente', 'pagado', 'cancelado'],
+        default: 'pendiente'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Payment', PaymentSchema);
