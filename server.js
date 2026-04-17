2b9d3b64e86efce3ead5d9640fc2d85aa1d5d107
imacol <redimacol.re@gmail.com>
2026-04-17 15:28:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const paymentRoutes = require('./routes/payments');
+const expenseRoutes = require('./routes/expenses');
+const reportRoutes = require('./routes/reports');
 
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Database Configuration
+mongoose.connect('mongodb+srv://rooot:TU_PASSWORD@gestor-pagos.rbbjl7w.mongodb.net/gestor-pagos', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected!'))
+    .catch(err => console.log(err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/payments', paymentRoutes);
+app.use('/api/expenses', expenseRoutes);
+app.use('/api/reports', reportRoutes);
+
+// Server Setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
