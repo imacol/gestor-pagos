84a9d69be5cc0a2e4eb1a267deffceb4eb6d5f36
imacol <redimacol.re@gmail.com>
2026-04-17 20:23:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://admin:Admin1234@gestor-pagos.rbbjl7w.mongodb.net/gestor-pagos', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB Connected!')).catch(err => console.log(err));
+mongoose.connect('mongodb+srv://admin:Admin1234@gestor-pagos.rbbjl7w.mongodb.net/gestor-pagos')
+    .then(() => console.log('MongoDB Connected!'))
+    .catch(err => console.log(err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/payments', paymentRoutes);
